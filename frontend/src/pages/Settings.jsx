15521def85ecd7d5ee01dd82da98181349828146
aljaz-ferenc <aljaz.ferenc@protonmail.com>
2023-12-13 22:24:07 +0100

    update tile providers

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -27,7 +27,7 @@ const maps = [
   {
     image: map3,
     provider:
-      "https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.png",
+    'https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png',
     name: "Toner",
   },
   {
